0f3b6b4af90010471dde95e0c50a91ebe84c4074
MrFreuden <mrfreuden@gmail.com>
2025-10-08 20:56:01 +0200

    Удаление загрузки данных по умолчанию и добавление AdminSync
    В файле `Program.cs` закомментированы вызовы методов для загрузки данных по умолчанию, что может улучшить производительность приложения.
    
    В файле `AdminSync.cs` добавлен новый класс `AdminSync`, который реализует команду для синхронизации кэша с базой данных, включая зависимости `IUserRepository` и `RepositoryLoader`. Это расширяет функциональность приложения для администрирования.

---
+++ EventRegistrator/Application/Commands/AdminSync.cs
@@ -0,0 +1,34 @@
+﻿using EventRegistrator.Application.Commands.Attributes;
+using EventRegistrator.Application.DTOs;
+using EventRegistrator.Domain.DTO;
+using EventRegistrator.Domain.Interfaces;
+using EventRegistrator.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventRegistrator.Application.Commands
+{
+    [Command("/sync", "Синхронизация кэша с бд")]
+    public class AdminSync : AdminCommandBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly RepositoryLoader _repositoryLoader;
+
+        public AdminSync(RepositoryLoader repositoryLoader, IUserRepository userRepository)
+        {
+            _repositoryLoader = repositoryLoader;
+            _userRepository = userRepository;
+        }
+
+        protected async override Task<List<Response>> ExecuteAdminCommand(MessageDTO message)
+        {
+            var user = _userRepository.GetUserByTargetChat(message.ChatId);
+            var rep = _repositoryLoader.LoadData();
+            var command = new AdminSaveCommand(rep, _repositoryLoader);
+            return await command.Execute(message, user);
+        }
+    }
+}
